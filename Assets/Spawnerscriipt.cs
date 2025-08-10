9dfa9e3a40dbf903c1e05c4d09ff9a627e5c56d8
Akram Shammi <akramshammi@gmail.com>
2025-08-10 14:48:14 +0530

    .

--- Assets/Spawnerscriipt.cs
+++ Assets/Spawnerscriipt.cs
@@ -5,10 +5,11 @@ public class Spawnerscriipt : MonoBehaviour
     public GameObject Pipe;
     public float spawnRate = 2;
     private float Timer = 0;
+    public float heightOffset = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spawnPipe();
     }
 
     // Update is called once per frame
@@ -17,8 +18,15 @@ public class Spawnerscriipt : MonoBehaviour
         if(Timer<spawnRate){
             Timer+= Time.deltaTime;        }
             else{
-                Instantiate(Pipe, transform.position,transform.rotation);
+                spawnPipe();
                 Timer = 0;
             }
+    }
+    void spawnPipe(){
+
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+
+         Instantiate(Pipe, new Vector3(transform.position.x,Random.Range(lowestPoint,highestPoint),0),transform.rotation);
     }
 }
--- Assets/pipeMovement.cs
+++ Assets/pipeMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class pipeMovement : MonoBehaviour
 {
     public float moveSpeed = 5;
+    public float deadZone = -45;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,5 +14,9 @@ public class pipeMovement : MonoBehaviour
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if(transform.position.x<deadZone){
+
+            Destroy(gameObject);
+        }
     }
 }
